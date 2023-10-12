49222d2e1da90c92ebb9798a04a1108a78721fac
silent-rain <2367221387@qq.com>
2023-10-12 21:43:05 +0800

    完善TIM2 定时器延迟示例

--- src/bin/tim2_delay.rs
+++ src/bin/tim2_delay.rs
@@ -16,7 +16,7 @@ fn main() -> ! {
     // 从外围设备访问机箱访问设备特定的外围设备
     let dp = Peripherals::take().unwrap();
     // 从 cortex-m 机箱访问核心外围设备
-    let cp = CorePeripherals::take().unwrap();
+    let _cp = CorePeripherals::take().unwrap();
 
     let rcc = &dp.RCC;
     let flash = &dp.FLASH;
@@ -38,8 +38,8 @@ fn main() -> ! {
     loop {
         for i in 0..10 {
             println!("i={:?}", i);
-            // 延时一秒
-            delay.delay_ms(1000);
+            // 延时
+            delay.delay_ms(2000);
         }
     }
 }
@@ -49,15 +49,27 @@ struct Delay {
 }
 
 impl Delay {
+    /// 预分频值 (PSC) 是用来设置 TIM2 的输入时钟频率的。
+    /// STM32F1 的 TIM2 的输入时钟频率是 72 MHz，也就是说每秒有 72000000 个时钟周期。
+    /// 如果你想让 TIM2 的计数频率降低一些，你可以设置一个预分频值，让输入时钟除以这个值再输入到 TIM2。
+    /// 例如，如果你设置 PSC 为 8，那么 TIM2 的计数频率就是 72 MHz / 8 = 9 MHz，也就是说每秒有 9000000 次计数。
+    ///
+    /// 延时时间 = PSC x ARR / 输入时钟频率
     fn delay_ms(&mut self, ms: u16) {
         // 设置 TIM2 的预分频器和自动重装载寄存器
-        self.tim.psc.write(|w| w.psc().bits(7999)); // 预分频器
-        self.tim.arr.write(|w| w.arr().bits(1000)); // 自动重装载寄存器
+        // 预分频器
+        self.tim.psc.write(|w| w.psc().bits(8000));
+        // 自动重装载寄存器
+        // 72_000_000 / 8000 / 1000(ms)
+        self.tim.arr.write(|w| w.arr().bits(9 * ms));
 
-        self.tim.cr1.modify(|_, w| w.cen().set_bit()); // 启动 TIM2
+        // 启动 TIM2
+        self.tim.cr1.modify(|_, w| w.cen().set_bit());
 
-        while self.tim.sr.read().uif().bit_is_clear() {} // 等待更新事件标志
+        // 等待更新事件标志
+        while self.tim.sr.read().uif().bit_is_clear() {}
 
-        self.tim.sr.modify(|_, w| w.uif().clear_bit()); // 清除更新事件标志
+        // 清除更新事件标志
+        self.tim.sr.modify(|_, w| w.uif().clear_bit());
     }
 }
